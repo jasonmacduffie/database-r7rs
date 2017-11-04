68fe8da6229d0fc7b18ef1d58f2194d90e61b59e
Jason K. MacDuffie <taknamay@gmail.com>
2017-11-03 22:06:48 -0400

    init commit

---
+++ database.scm
@@ -0,0 +1,80 @@
+
+(import
+  (scheme base)
+  (srfi 1))
+
+(define (assq-ref al key)
+  (cdr (assq key al)))
+
+(define (select keys list-of-alists)
+  (map (lambda (al)
+         (if (eq? keys '*)
+             al
+             (map (lambda (key)
+                    (cons key (assq-ref al key)))
+                  keys)))
+       list-of-alists))
+
+(define (join pred la lb)
+  (let a-loop ((in-a la)
+               (out-ab '()))
+    (if (null? in-a)
+        out-ab
+        (let b-loop ((in-b lb)
+                     (out-b out-ab))
+          (if (null? in-b)
+              (a-loop (cdr in-a) out-b)
+              (b-loop (cdr in-b)
+                      (if (pred (car in-a)
+                                (car in-b))
+                          (cons (append (car in-a)
+                                        (car in-b))
+                                out-b)
+                          out-b)))))))
+
+(define (table->alists t)
+  (define column-names (car t))
+  (define table-body (cddr t))
+  (map (lambda (row)
+         (map (lambda (name col)
+                (cons name col))
+              column-names
+              row))
+       table-body))
+
+(define (insert-rows! t als)
+  (define column-names (car t))
+  (define new-rows
+    (map (lambda (al)
+           (map cdr al))
+         (select column-names als)))
+  (set-cdr! (cdr t)
+            (append new-rows (cddr t))))
+
+(define (insert-into! db tb als)
+  (define table (select-table db tb))
+  (insert-rows! table als))
+
+(define (update-row! col-names old-row value-al)
+  (for-each (lambda (p)
+              (list-set!
+               old-row
+               (list-index (lambda (name)
+                             (eq? name (car p)))
+                           col-names)
+               (cdr p)))
+            value-al))
+
+(define (update-table! db tb proc)
+  (define table (select-table db tb))
+  (define (row-mapping! row)
+    (update-row! (car table)
+                 row
+                 (proc (map cons
+                            (car table)
+                            row))))
+   (for-each row-mapping! (cddr table)))
+
+(define (select-table db tb)
+  (assq-ref (cdr db) tb))
+
---
+++ testing.scm
@@ -0,0 +1,37 @@
+
+(import
+  (scheme base)
+  (scheme read)
+  (scheme write)
+  (scheme load))
+
+(define database
+  '(db
+    (people
+     (name age)
+     (string integer)
+     ("Alice" 25)
+     ("Bob" 50)
+     ("Cathy" 15))
+    (pets
+     (pet-name species owner-name)
+     ("Agatha" "bird" "Alice")
+     ("Roger" "dog" "Cathy")
+     ("Kiddo" "cat" "Alice")
+     ("Kudos" "dog" "Ricard"))))
+
+(define people
+  (table->alists (assq-ref (cdr database) 'people)))
+
+(define pets
+  (table->alists (assq-ref (cdr database) 'pets)))
+
+(define query
+  (select
+   '(name pet-name)
+   (join (lambda (person pet)
+           (equal? (assq-ref person 'name)
+                   (assq-ref pet 'owner-name)))
+         people
+         pets)))
+
